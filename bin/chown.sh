078cfede21c7c38968195b4a1a419857f4c2c544
Lev Shagalov <lewik@interprogma.ru>
2014-06-06 15:03:24 +0400

    chown

--- bin/chown.sh
+++ bin/chown.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
 echo "chown"
-cd /var/www
-chown www-data:www-data -R *
+if [ ${erzakaz[enable]} ]; then
+	echo "erzakaz"
+	cd ${erzakaz[localfolder]}
+	chown www-data:www-data -R *
+fi
+if [ ${erreestr[enable]} ]; then
+	echo "erreestr"
+	cd ${erreestr[localfolder]}
+	chown www-data:www-data -R *
+fi
+if [ ${ercommon[enable]} ]; then
+	echo "ercommon"
+	cd ${ercommon[localfolder]}
+	chown www-data:www-data -R *
+fi
+if [ ${erportal[enable]} ]; then
+	echo "erportal"
+	cd ${erportal[localfolder]}
+	chown www-data:www-data -R *
+fi
+if [ ${ersf[enable]} ]; then
+	echo "ersf"
+	cd ${ersf[localfolder]}
+	chown www-data:www-data -R *
+fi
+
 echo "chown ok"
