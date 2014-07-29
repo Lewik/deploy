5a669b9847e99d8161aee902778fcf729233899c
Lev Shagalov <lllewik@gmail.com>
2014-07-29 12:23:59 +0400

    Добавлен scp

---
+++ bin/scp.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+ROOT_PATH=$(cd $(dirname $0) && pwd);
+echo "get via scp"
+
+#создаем и очищаем папку для архива
+mkdir -pv ${scp_folder}
+cd ${scp_folder}
+rm -rf *
+
+#скачиваем архив
+scp -P ${scp_port} ${scp_url} ${scp_folder}
+
+#получить имя архива
+cd ${scp_folder}
+
+for file in ./*; do
+  ZIP=${file##*/}
+  break
+done
+
+FILE=${ZIP%.*}
+
+#извлечение данных
+unzip -x $ZIP
+
+#переместить в Debug & Release
+rm -rf $FILE/ersf/vendor
+
+mkdir -pv ${srv_debug}
+cp -av $FILE/* ${srv_debug}
+mkdir -pv ${srv_prod}
+cp -av $FILE/* ${srv_prod}
+
+rm -rf ${scp_folder}
+
+echo "ready"
+cd ${ROOT_PATH}
---
+++ scp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ./bin/settings.conf.sh
+source ./bin/scp.sh
